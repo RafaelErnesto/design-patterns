04b7fadd4f37c51df000d77b2607d1e97041551d
Rafael <rflernesto@gmail.com>
2021-05-21 20:10:48 -0400

    feature: creates new contracts

--- typescript/src/chainOfResponsability/contracts/TranslatorHandleInterface.ts
+++ typescript/src/chainOfResponsability/contracts/TranslatorHandleInterface.ts
@@ -1,5 +1,6 @@
 import { TranslationRequest } from "../../customTypes/TranslationRequest";
 
 export interface TranslatorHandleInterface {
+    setNextHandler(nextHandler: TranslatorHandleInterface): void
     translate(request: TranslationRequest): void
 }
